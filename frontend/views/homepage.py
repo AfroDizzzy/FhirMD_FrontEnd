5413d3ffd169cda986707894428706da1a4f52c2
AfroDizzzy <davidrhode92@gmail.com>
2025-06-29 23:00:54 +1200

    updated homepage to only display the condition and the section

--- frontend/views/homepage.py
+++ frontend/views/homepage.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 import json
 from frontend.models.generalCategories import generalCategories
-
+from frontend.models.readingFromJsonFile import *
 def homepage(request):
-
     context = {
-        # 'items': SAMPLE_ITEMS,
-        'items': generalCategories,
+         'items': SAMPLE_ITEMS,
+        # 'items': generalCategories,
         'page_title': 'Django Home Page'
     }
     return render(request, 'frontend/homepage.html', context)
