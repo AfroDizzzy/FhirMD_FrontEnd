4907d9e8caa8fce1f7e18c549e398324e6466de0
AfroDizzzy <davidrhode92@gmail.com>
2025-06-30 02:34:10 +1200

    added section stripping from the json, and sorted it

--- frontend/models/readingFromJsonFile.py
+++ frontend/models/readingFromJsonFile.py
@@ -1,5 +1,15 @@
 import json
+import re
 
 SAMPLE_ITEMS: json
+
 with open('testData.json') as jsonFile:
     SAMPLE_ITEMS = json.load(jsonFile)
+
+sections = set()
+for j in SAMPLE_ITEMS:
+    sectionNameSeperated = sectionNameSeperated = re.sub(r"(\w)([A-Z])", r"\1 \2", j['resourceType'])
+    j['resourceType'] = sectionNameSeperated
+    sections.add(j['resourceType'])
+    
+LIST_OF_SECTIONS = sorted(sections)
