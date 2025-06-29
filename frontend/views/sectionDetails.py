2907378dc5c0297ddd0c905ec6f55064068223f8
AfroDizzzy <davidrhode92@gmail.com>
2025-06-30 02:35:56 +1200

    added details of each item

--- frontend/views/sectionDetails.py
+++ frontend/views/sectionDetails.py
@@ -1,11 +1,12 @@
 from django.http import Http404
 from django.shortcuts import redirect, render
 from frontend.models.readingFromJsonFile import *
-
+import re
 
 def sectionDetails(request, sectionDetails):
     sectionsInCategory: list = []
 
+# this needs to go into the startup of the program until we go into
     for j in SAMPLE_ITEMS:
         if j['resourceType'] == sectionDetails: 
             sectionsInCategory.append(j)
@@ -14,7 +15,8 @@ def sectionDetails(request, sectionDetails):
         raise  Http404()
     
     context = {
-        'sectionDetails': sectionsInCategory,
+        'sectionName': sectionDetails,
+        'items': sectionsInCategory,
         'page_title': 'sections page'
     }
     return render(request, 'frontend/sectionDetails.html', context)
