087fb5f7b278b93b9e65680f6c2450925f11401f
AfroDizzzy <davidrhode92@gmail.com>
2025-06-30 14:09:36 +1200

    dockerised the app

--- FHIRMD/settings.py
+++ FHIRMD/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tm8@hx!_g+snx!p8z4*haej4v@m&c_f2r1amxg6d$h)3ue$vu(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
