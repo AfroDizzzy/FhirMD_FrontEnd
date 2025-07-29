64505d5be6bdf9082fea581b66980c1b637603b1
AfroDizzzy <davidrhode92@gmail.com>
2025-07-29 16:05:49 +1200

    Hard coded the sections

--- frontend/views/homepage.py
+++ frontend/views/homepage.py
@@ -4,19 +4,6 @@ from django.http import HttpResponse
 
 def homepage(request):
     context = {
-        "pages": {
-            "sections": {
-                "name": "Sections",
-                "description": "Sections page ya dummy"
-            },
-            "sections":{
-                "name": "Sssections",
-                "description": "Sections page ya dummy"
-            }
-        
-        # "references",
-        # "ingrediants?" 
-        }
     }
     
     return render(request, "frontend/homePage.html", context)
