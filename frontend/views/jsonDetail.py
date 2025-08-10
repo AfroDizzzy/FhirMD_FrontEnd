ad6137f1fb28e0dfcf5ef797cc429eb6c86e613f
AfroDizzzy <davidrhode92@gmail.com>
2025-08-11 00:35:53 +1200

    added metadata to json details area

--- frontend/views/jsonDetail.py
+++ frontend/views/jsonDetail.py
@@ -8,19 +8,23 @@ def json_detail(request, item_id):
     Displays the pretty-formatted JSON for a specific item by its ID.
     """
     found_item = None
+    metadata = None
     for item in SAMPLE_ITEMS:
         # Assuming each item has a unique 'id' field
         if 'id' in item and item['id'] == item_id:
             found_item = item
             break
 
+        metadata = item.get('file_metadata')
+    print(metadata)
     if found_item:
         pretty_json = json.dumps(found_item, indent=4)
         context = {
             'item_id': item_id,
             'DisplayName': item_id.replace('-', ' ').replace('_', ' ').title(),
             'pretty_json': pretty_json,
-            'resource_type': found_item.get('resourceType', 'N/A')
+            'resource_type': found_item.get('resourceType', 'N/A'),
+            'file_metadata': metadata
         }
         return render(request, 'frontend/json_detail.html', context)
     else:
